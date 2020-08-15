b28c47a59bfd830580ce46fe77161d1ec12bb899
msteinborn <steinborn.matthew@gmail.com>
2020-08-15 15:13:25 -0400

    a little more

--- woodRaptor.py
+++ woodRaptor.py
@@ -16,10 +16,13 @@ centers = currentRap.loc[[
                                 'Joel Embiid',
                                 'Clint Capela',
                                 'Steven Adams',
-                                'Andre Drummond'
+                                'Andre Drummond',
+                                'Hassan Whiteside',
+                                'Jarrett Allen',
+                                'Rudy Gobert'
 ]]
 
-centers = centers.loc[:, ['mp', 'poss', 'war_reg_season', 'raptor_total']]
+centers = centers.loc[:, ['mp', 'poss', 'raptor_offense', 'raptor_defense', 'raptor_total']]
 
 ax1 = centers.plot.scatter(x='poss',
 
@@ -30,13 +33,17 @@ ax1 = centers.plot.scatter(x='poss',
 for k, v in centers.loc[:,['poss', 'raptor_total']].iterrows():
     ax1.annotate(k, v)
 
-ax2 = centers.plot.scatter(x ='raptor_total',
-                           y='war_reg_season')
+ax2 = centers.plot.scatter(x ='raptor_defense',
+                           y='raptor_offense')
 
-for k, v in centers.loc[:,['raptor_total', 'war_reg_season']].iterrows():
+for k, v in centers.loc[:,['raptor_defense', 'raptor_offense']].iterrows():
                                ax2.annotate(k, v)
 
 
-
+ax2.vlines(0,-10,10,colors='red')
+ax2.hlines(0,-10,10,colors='red')
+ax2.set_xlim(-1,8)
+ax2.set_ylim(-3,6)
+ax2.grid()
 
 plt.show()
